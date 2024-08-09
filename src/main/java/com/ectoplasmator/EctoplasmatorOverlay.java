61a3d5991c5c6ed80bfc9522f6320eba92f84ba0
staytheknight <staytheknight@gmail.com>
2024-08-09 16:15:31 -0600

    got config toggles to work
    Had to figure out a system to make them all work at the same time.
    
    added them to a boolean list, each toggle adds some condition to a list.
    
    The list is iterated through, if any of the toggles are false, it breaks. They must all be true for the render to trigger

--- src/main/java/com/ectoplasmator/EctoplasmatorOverlay.java
+++ src/main/java/com/ectoplasmator/EctoplasmatorOverlay.java
@@ -3,21 +3,22 @@ package com.ectoplasmator;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import javax.inject.Inject;
+import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.InventoryID;
-import net.runelite.api.Item;
 import net.runelite.api.ItemID;
 import static net.runelite.api.ItemID.*;
 import net.runelite.api.NPC;
 import net.runelite.api.Perspective;
 import net.runelite.api.Point;
 import net.runelite.api.coords.LocalPoint;
+import net.runelite.api.events.HitsplatApplied;
+import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.game.ItemManager;
-import net.runelite.client.plugins.cluescrolls.clues.item.AnyRequirementCollection;
-import net.runelite.client.plugins.cluescrolls.clues.item.ItemRequirement;
-import static net.runelite.client.plugins.cluescrolls.clues.item.ItemRequirements.item;
 import net.runelite.client.ui.overlay.Overlay;
 import net.runelite.client.ui.overlay.OverlayLayer;
 import net.runelite.client.ui.overlay.OverlayPosition;
@@ -35,6 +36,11 @@ class EctoplasmatorOverlay extends Overlay
 	private final EctoplasmatorPlugin plugin;
 	private final ItemManager itemManager;
 
+	// Display variables
+	private boolean inCombat;									// Boolean for if the player is in combat
+	private List<Boolean> displayChecks = new ArrayList<>();	// Boolean array of display toggles
+	private boolean displayToggle;								// Final display toggle boolean
+
 	// Class Constructor
 	@Inject
 	private EctoplasmatorOverlay(Client client, EctoplasmatorPlugin plugin, ItemManager itemManager)
@@ -44,6 +50,31 @@ class EctoplasmatorOverlay extends Overlay
 		this.itemManager = itemManager;
 		setPosition(OverlayPosition.DYNAMIC);
 		setLayer(OverlayLayer.UNDER_WIDGETS);
+
+		// sets default render toggle to be true
+		displayToggle = true;
+	}
+
+	// Triggers when a hitsplat is detected in the world
+	@Subscribe
+	public void onHitsplatApplied(HitsplatApplied event) throws InterruptedException
+	{
+		// Filters hitsplats so that it only triggers for the player
+		if (event.getActor() != client.getLocalPlayer())
+		{
+			return;
+		}
+
+		// Sets the in combat statue to true, and calls the combat timer
+		inCombat = true;
+		combatTimer();
+	}
+
+	// Combat timer sets the combat statue to false after 10 seconds
+	public void combatTimer() throws InterruptedException
+	{
+		TimeUnit.SECONDS.sleep(10);
+		inCombat = false;
 	}
 
 	@Override
@@ -65,7 +96,49 @@ class EctoplasmatorOverlay extends Overlay
 			return null;
 		}
 
+		// Clears the boolean array on each render loop
+		if(!displayChecks.isEmpty())
+		{
+			displayChecks.clear();
+		}
 
+		// Adds different display toggles based on configuration settings to the display check boolean array
+		if (config.hideIfInventory())
+		{
+			displayChecks.add(!client.getItemContainer(InventoryID.INVENTORY).contains(ECTOPLASMATOR));
+		}
+		if (config.onlyInCombat())
+		{
+			displayChecks.add(inCombat);
+		}
+
+		// Iterates through the display check boolean array to see if any of the booleans are false
+		// If they are false set the master toggle to false and break
+		for (boolean b : displayChecks)
+		{
+			if(!b)
+			{
+				displayToggle = false;
+				break;
+			}
+			else
+			{
+				displayToggle = true;
+			}
+		}
+
+		if(displayChecks.isEmpty())
+		{
+			displayToggle = true;
+		}
+
+		// If the master display toggle is true, display the overlay
+		if(displayToggle)
+		{
+			renderOverlay(targets, graphics, image);
+		}
+
+		/*
 		// If the player does not have an Ectoplasmator in their inventory - then render overlay;
 		if (config.hideIfInventory())
 		{
@@ -73,10 +146,12 @@ class EctoplasmatorOverlay extends Overlay
 			{
 				renderOverlay(targets, graphics, image);
 			}
-
-		} else {
-			renderOverlay(targets, graphics, image);
 		}
+
+		else
+		{
+			renderOverlay(targets, graphics, image);
+		}*/
 
 		return null;
 	}
