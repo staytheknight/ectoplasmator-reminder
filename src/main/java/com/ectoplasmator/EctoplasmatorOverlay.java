c63f11180a55c168f123d4eb2c09fda7af36a4bc
staytheknight <staytheknight@gmail.com>
2024-08-09 11:07:16 -0600

    Testing render
    No filters, should render over every NPC

--- src/main/java/com/ectoplasmator/EctoplasmatorOverlay.java
+++ src/main/java/com/ectoplasmator/EctoplasmatorOverlay.java
@@ -2,26 +2,41 @@ package com.ectoplasmator;
 
 import java.awt.Dimension;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.util.List;
 import javax.inject.Inject;
+import net.runelite.api.Client;
 import net.runelite.api.Item;
+import net.runelite.api.ItemID;
 import static net.runelite.api.ItemID.*;
 import net.runelite.api.NPC;
+import net.runelite.api.Perspective;
+import net.runelite.api.Point;
+import net.runelite.api.coords.LocalPoint;
+import net.runelite.client.game.ItemManager;
 import net.runelite.client.plugins.cluescrolls.clues.item.AnyRequirementCollection;
 import net.runelite.client.plugins.cluescrolls.clues.item.ItemRequirement;
 import static net.runelite.client.plugins.cluescrolls.clues.item.ItemRequirements.item;
 import net.runelite.client.ui.overlay.Overlay;
+import net.runelite.client.ui.overlay.OverlayLayer;
+import net.runelite.client.ui.overlay.OverlayPosition;
+import net.runelite.client.ui.overlay.OverlayUtil;
 
 class EctoplasmatorOverlay extends Overlay
 {
+	private final Client client;
 	private final EctoplasmatorPlugin plugin;
+	private final ItemManager itemManager;
 
 	// Class Constructor
 	@Inject
-	private EctoplasmatorOverlay(EctoplasmatorPlugin plugin)
+	private EctoplasmatorOverlay(Client client, EctoplasmatorPlugin plugin, ItemManager itemManager)
 	{
-		super(plugin);
-		this.plugin=plugin;
+		this.client = client;
+		this.plugin = plugin;
+		this.itemManager = itemManager;
+		setPosition(OverlayPosition.DYNAMIC);
+		setLayer(OverlayLayer.UNDER_WIDGETS);
 	}
 
 	// Check to see if Ectoplasmator is in the player's inventory
@@ -31,7 +46,21 @@ class EctoplasmatorOverlay extends Overlay
 	@Override
 	public Dimension render(Graphics2D graphics)
 	{
+		// Gets the image for the Ectoplasmator to be rendered
+		final BufferedImage image = itemManager.getImage(ItemID.ECTOPLASMATOR);
+		// Error Catch if the image is null
+		if (image == null)
+		{
+			return null;
+		}
+
+		// Gets list of NPCS to render the image above
 		final List<NPC> targets = plugin.getNPCTargets();
+		// Error catch for if the npc target list is empty
+		if (targets.isEmpty())
+		{
+			return null;
+		}
 
 		// Gets all the items in the player's inventory
 		final Item[] inventoryItems = plugin.getInventoryItems();
@@ -39,7 +68,31 @@ class EctoplasmatorOverlay extends Overlay
 		// If the player does not have an Ectoplasmator in their inventory - then render overlay;
 		if (!HAS_ECTOPLASMATOR.fulfilledBy(inventoryItems))
 		{
+			for (NPC target : targets)
+			{
+				renderTargetItem(graphics, target, image);
+			}
+
 		}
 		return null;
+	}
+
+	private void renderTargetItem(Graphics2D graphics, NPC actor, BufferedImage image)
+	{
+		final LocalPoint actorPosition = actor.getLocalLocation();
+		// TODO: set +40 offset to config
+		final int offset = actor.getLogicalHeight() + 40;
+
+		if (actorPosition == null || image == null)
+		{
+			return;
+		}
+
+		final Point imageLoc = Perspective.getCanvasImageLocation(client, actorPosition, image, offset);
+
+		if (imageLoc != null)
+		{
+			OverlayUtil.renderImageLocation(graphics, imageLoc, image);
+		}
 	}
 }
--- src/main/java/com/ectoplasmator/EctoplasmatorPlugin.java
+++ src/main/java/com/ectoplasmator/EctoplasmatorPlugin.java
@@ -107,5 +107,4 @@ public class EctoplasmatorPlugin extends Plugin
 	}
 
 
-
 }
