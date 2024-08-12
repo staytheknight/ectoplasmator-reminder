7cc2699ca76c768136227789d53655c12b52b05c
staytheknight <staytheknight@gmail.com>
2024-08-12 15:32:42 -0600

    Config Sections for better organization

--- src/main/java/com/ectoplasmator/EctoplasmatorConfig.java
+++ src/main/java/com/ectoplasmator/EctoplasmatorConfig.java
@@ -3,15 +3,25 @@ package com.ectoplasmator;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
 
 @ConfigGroup("example")
 public interface EctoplasmatorConfig extends Config
 {
+	// Display Behaviour Section
+	@ConfigSection(
+		name = "Display Behaviour",
+		description = "Various in game behaviours to control when the Ectoplasmator overlay is displayed",
+		position = 0
+	)
+	String displayBehaviourSection = "Display Behaviour";
+
 	@ConfigItem(
 		position = 1,
 		keyName = "hideWhileInInventory",
 		name = "Hide overlay if in Inventory",
-		description = "Hides the Ectoplasmator overlay if it's in player's inventory"
+		description = "Hides the Ectoplasmator overlay if it's in player's inventory",
+		section = displayBehaviourSection
 	)
 	default boolean hideIfInventory()
 	{
@@ -22,7 +32,8 @@ public interface EctoplasmatorConfig extends Config
 		position = 2,
 		keyName = "displayOnlyInCombat",
 		name = "Display Only In Combat",
-		description = "Display the Ectoplasmator overlay only while in combat"
+		description = "Display the Ectoplasmator overlay only while in combat",
+		section = displayBehaviourSection
 	)
 	default boolean onlyInCombat()
 	{
@@ -33,18 +44,28 @@ public interface EctoplasmatorConfig extends Config
 		position = 3,
 		keyName = "hideInWilderness",
 		name = "Hide Overlay in Wilderness",
-		description = "Hide the Ectoplasmator overlay while in the wilderness"
+		description = "Hide the Ectoplasmator overlay while in the wilderness",
+		section = displayBehaviourSection
 	)
 	default boolean hideInWilderness()
 	{
 		return false;
 	}
 
+	// Location & Scale Section
+	@ConfigSection(
+		name = "Display Location & Scale",
+		description = "Controls to change the size and vertical location of the Ectoplasmator overlay",
+		position = 4
+	)
+	String displayControls = "Display Location & Scale";
+
 	@ConfigItem(
-		position = 4,
+		position = 5,
 		keyName = "overlayVerticalOffset",
 		name = "Overlay Vertical Offset",
-		description = "How high above the NPCs the overlay is displayed, this is to customize location in case it's conflicting with the position of other addons."
+		description = "How high above the NPCs he overlay is displayed, this is to customize location in case it's conflicting with the position of other addons.",
+		section = displayControls
 	)
 	default int verticalOffset()
 	{
@@ -52,10 +73,11 @@ public interface EctoplasmatorConfig extends Config
 	}
 
 	@ConfigItem(
-		position = 5,
+		position = 6,
 		keyName = "overlayScale",
 		name = "Overlay Scale",
-		description = "Scale (size) of the Ectoplasmator overlay"
+		description = "Scale (size) of the Ectoplasmator overlay",
+		section = displayControls
 	)
 	default int overlayScale()
 	{
